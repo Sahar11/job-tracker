36c3b5012d1c7ca3ff41013b42bc346ae39a02ad
sahar11 <saharhalim151@gmail.com>
2025-10-02 09:12:41 -0400

    added login file in frontend

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,13 +2,16 @@
 import jwt from "jsonwebtoken";
 
 export const authMiddleware = (req, res, next) => {
+    const authHeader = req.headers["authorization"];
+    if(!authHeader) return res.status(401).json({message: "No token provided" });
+
     const token = req.headers["authorization"]?.split(" ")[1];
-    if(token) return res.status(401).json({error: "Access Denied"});
+    if(!token) return res.status(401).json({error: "Token Missing"});
 
     try{
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded.id;
+        req.user = decoded;
         next();
 
     } catch (err){
